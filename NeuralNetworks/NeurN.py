791750ff7b551135d1405d2abf80f3c351c1960e
Jalal <128756237+JalalA984@users.noreply.github.com>
2023-07-09 18:26:34 -0400

    Test and predict using network and model

--- NeuralNetworks/NeurN.py
+++ NeuralNetworks/NeurN.py
@@ -15,3 +15,26 @@ labels = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 # Scaling down data
 train_images = train_images/255.0
 test_images = test_images/255.0
+
+# Setting up model and neural network
+
+# define layers
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+# Using our model
+prediction = model.predict(test_images)
+
+for x in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[x], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + labels[test_labels[x]])
+    plt.title("Prediction: " + labels[np.argmax(prediction[x])])
+    plt.show()
