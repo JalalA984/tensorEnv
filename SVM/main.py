c5660aca4840473d03b43643b77747867683fffe
Jalal <128756237+JalalA984@users.noreply.github.com>
2023-07-08 20:03:09 -0400

    Complete SVM model

--- SVM/main.py
+++ SVM/main.py
@@ -1,6 +1,8 @@
 import sklearn
 from sklearn import datasets
 from sklearn import svm
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
 
 # Cancer (classification) dataset: features point to tumor that is cancerous or not
 cancer = datasets.load_breast_cancer()
@@ -12,7 +14,20 @@ labels = cancer.target_names
 x = cancer.data
 y = cancer.target
 
-# Split data into train and test groups
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+for _ in range(10):
+    # Split data into train and test groups
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
 
-classes = ["malignant", "benign"]
+    classes = ["malignant", "benign"]
+
+    # Our classifier
+    clsfier = svm.SVC(kernel="linear", C=2)
+
+    # Train data
+    clsfier.fit(x_train, y_train)
+
+    y_pred = clsfier.predict(x_test)
+
+    # Test accuracy of our predictions to actual label results
+    acc = metrics.accuracy_score(y_test, y_pred) # changes each time program is run
+    print(acc)
